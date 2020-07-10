1e3917f49086c2500db8d3e5bf4f03693aa34bc9
Omar Yasser <omaryassersalah@outlook.com>
2020-07-11 00:54:59 +0200

    - initial commit to the repo.

---
+++ app/src/main/java/com/orange/internship/notificationsproofofconcept/UI/MainActivity.kt
@@ -0,0 +1,12 @@
+    package com.orange.internship.notificationsproofofconcept.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.orange.internship.notificationsproofofconcept.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
